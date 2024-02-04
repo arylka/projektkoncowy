1385c883b84855931c1a350003b7b8896ab6a024
arylka <yukinowak8@gmail.com>
2024-02-04 20:02:38 +0100

    'modele'

--- projectdjango/wsgi.py
+++ projectdjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projektkoncowy project.
+WSGI config for projectdjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projektkoncowy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectdjango.settings')
 
 application = get_wsgi_application()
